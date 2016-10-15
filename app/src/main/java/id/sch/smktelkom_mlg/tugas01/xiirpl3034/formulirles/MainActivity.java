1bb6a9d9680ecff167eca9779f1b861689480364
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-10-15 07:45:53 -0700

    Deklarasi dan Inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3034/formulirles/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3034/formulirles/MainActivity.java
@@ -2,12 +2,37 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3034.formulirles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    Spinner spKelas;
+    RadioGroup rgJenjang;
+    // RadioButton rdSD, rdSMP, rdSMA;
+    CheckBox cbSenin, cbRabu, cbJumat;
+    TextView tvHasil, tvHasil1, tvPilihH, tvHari;
+    int nHari;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        spKelas = (Spinner) findViewById(R.id.spinnerKelas);
+        rgJenjang = (RadioGroup) findViewById(R.id.radioGroupJenjang);
+        //rdSD = (RadioButton) findViewById(R.id.radioButtonSD);
+        cbSenin = (CheckBox) findViewById(R.id.checkBoxSenin);
+        cbRabu = (CheckBox) findViewById(R.id.checkBoxRabu);
+        cbJumat = (CheckBox) findViewById(R.id.checkBoxJumat);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil1 = (TextView) findViewById(R.id.textViewHasil1);
+        tvHari = (TextView) findViewById(R.id.textViewHari);
+        tvPilihH = (TextView) findViewById(R.id.textViewPilihH);
+
     }
 }
